23c7e060daa96c3535a32847ba502fe09e62015b
selinozluk <selinozluk36@gmail.com>
2025-06-17 09:52:07 +0300

    Add project files.

---
+++ Patika.Net/Program.cs
@@ -0,0 +1,20 @@
+﻿//İngilizce karakter kullanılmayabilir fakat doğru bir çıktı alabiliyorum.
+Console.WriteLine("Selin Özlük");  
+Console.WriteLine("Normalde python yazıyordum ve yapay zeka alanında ilerlemek istiyordum fakat artık C# programlama dilinde ve frameworklerde kendimi geliştirmeyi hedefliyorum.");
+
+
+/* Bu şekilde de yazılabilir ikisi de çalışıyor
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Selin Özlük");
+        Console.WriteLine("Normalde python yazıyordum ve yapay zeka alanında ilerlemek istiyordum fakat artık C# programlama dilinde ve frameworklerde kendimi geliştirmeyi hedefliyorum.");
+    }
+}
+
+*/
