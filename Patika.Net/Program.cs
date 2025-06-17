8c386caf7182836e9ed4fd108df2f568f9b56261
selinozluk <selinozluk36@gmail.com>
2025-06-17 09:58:53 +0300

    updated the code

--- Patika.Net/Program.cs
+++ Patika.Net/Program.cs
@@ -1,11 +1,10 @@
-﻿//İngilizce karakter kullanılmayabilir fakat doğru bir çıktı alabiliyorum.
+﻿//Türkçe karakter kullanılmayabilir fakat doğru bir çıktı alabiliyorum.
 Console.WriteLine("Selin Özlük");  
 Console.WriteLine("Normalde python yazıyordum ve yapay zeka alanında ilerlemek istiyordum fakat artık C# programlama dilinde ve frameworklerde kendimi geliştirmeyi hedefliyorum.");
 
 
 /* Bu şekilde de yazılabilir ikisi de çalışıyor
 using System;
-using System.Text;
 
 class Program
 {
